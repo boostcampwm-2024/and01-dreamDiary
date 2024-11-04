51e8a07602b5557aa3886fd6481db77fcdb15d00
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-04 18:09:40 +0900

    chore: DiaryWriteScreen를 가진 NavHost 생성
    해당 NavHost를 가진 DreanDiaryApp 생성 및 MainActivity에서 이용

---
+++ app/src/main/java/com/boostcamp/dreamteam/dreamdiary/navigation/DreamDiaryNavHost.kt
@@ -0,0 +1,23 @@
+package com.boostcamp.dreamteam.dreamdiary.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.boostcamp.dreamteam.dreamdiary.feature.diary.write.DiaryWriteRoute
+import com.boostcamp.dreamteam.dreamdiary.feature.diary.write.diaryWriteScreen
+import com.boostcamp.dreamteam.dreamdiary.ui.DreamDiaryAppState
+
+@Composable
+fun DreamDiaryNavHost(
+    appState: DreamDiaryAppState,
+    modifier: Modifier = Modifier
+) {
+    val navController = appState.navController
+    NavHost(
+        navController = navController,
+        startDestination = DiaryWriteRoute,
+        modifier = modifier
+    ) {
+        diaryWriteScreen()
+    }
+}
---
+++ app/src/main/java/com/boostcamp/dreamteam/dreamdiary/ui/DreamDiaryAppState.kt
@@ -0,0 +1,23 @@
+package com.boostcamp.dreamteam.dreamdiary.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun rememberAppState(
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController: NavHostController = rememberNavController()
+) = DreamDiaryAppState(
+    coroutineScope = coroutineScope,
+    navController = navController,
+)
+
+@Stable
+class DreamDiaryAppState(
+    val navController: NavHostController,
+    coroutineScope: CoroutineScope
+)
