dc28618f545d9385722fbebbc0ce0bef5de8aa49
flashdragon <yooh3574@naver.com>
2024-11-05 16:25:32 +0900

    fix: 린트 포맷

--- feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/DiaryViewModel.kt
+++ feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/DiaryViewModel.kt
@@ -4,23 +4,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.boostcamp.dreamteam.dreamdiary.core.domain.usecase.GetDiariesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class DiaryViewModel
-    @Inject
-    constructor(
-        getDiariesUseCase: GetDiariesUseCase,
-    ) : ViewModel() {
-        private val _diaryHomeUIState: MutableStateFlow<DiaryHomeUIState> = MutableStateFlow(DiaryHomeUIState())
-        val diaryHomeUIState = _diaryHomeUIState.asStateFlow()
+class DiaryViewModel @Inject constructor(
+    getDiariesUseCase: GetDiariesUseCase,
+) : ViewModel() {
+    private val _diaryHomeUIState: MutableStateFlow<DiaryHomeUIState> =
+        MutableStateFlow(DiaryHomeUIState())
+    val diaryHomeUIState = _diaryHomeUIState.asStateFlow()
 
-        init {
-            viewModelScope.launch {
-                _diaryHomeUIState.value = DiaryHomeUIState(getDiariesUseCase(), false)
-            }
+    init {
+        viewModelScope.launch {
+            _diaryHomeUIState.value = DiaryHomeUIState(getDiariesUseCase(), false)
         }
     }
+}
