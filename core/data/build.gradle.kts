dcfe73c8c630d8d15e1f6ecf615d59248582fff8
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-06 17:57:01 +0900

    chore: room-paging 추가

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.paging)
 
     // firebase
     implementation(libs.firebase.firestore)
