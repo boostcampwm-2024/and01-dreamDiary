778ddf4a31fe22dd38a60c0d8bf003dca736c144
flashdragon <yooh3574@naver.com>
2024-11-06 10:57:48 +0900

    fix: 린트

--- core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/repository/DiaryRepository.kt
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/repository/DiaryRepository.kt
@@ -5,13 +5,13 @@ import com.boostcamp.dreamteam.dreamdiary.core.model.Diary
 import javax.inject.Inject
 
 class DiaryRepository @Inject constructor(
-        private val firebaseDataSource: FirebaseDataSource,
-    ) {
-        suspend fun getAllDiaryFromFireBase(): List<Diary> {
-            return firebaseDataSource.getAllDiaries()
-        }
-
-        fun setDiaryToFireBase(diary: Diary) {
-            firebaseDataSource.setDiary(diary)
-        }
+    private val firebaseDataSource: FirebaseDataSource,
+) {
+    suspend fun getAllDiaryFromFireBase(): List<Diary> {
+        return firebaseDataSource.getAllDiaries()
     }
+
+    fun setDiaryToFireBase(diary: Diary) {
+        firebaseDataSource.setDiary(diary)
+    }
+}
