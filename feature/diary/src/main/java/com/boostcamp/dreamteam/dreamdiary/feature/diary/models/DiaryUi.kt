7b6d0a402bb287b3e7c2a8d6947f6de84cd88c10
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-07 11:07:58 +0900

    fix: 시간 값을 임시 값 Instant.now 대신 Diary의 값을 사용하게 변경

--- feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/models/DiaryUi.kt
+++ feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/models/DiaryUi.kt
@@ -3,7 +3,6 @@ package com.boostcamp.dreamteam.dreamdiary.feature.diary.models
 import com.boostcamp.dreamteam.dreamdiary.core.model.Diary
 import com.boostcamp.dreamteam.dreamdiary.feature.diary.models.vos.DisplayableDateTime
 import com.boostcamp.dreamteam.dreamdiary.feature.diary.models.vos.toDisplayableDateTime
-import java.time.Instant
 
 data class DiaryUi(
     val id: String,
@@ -15,14 +14,13 @@ data class DiaryUi(
     val labels: List<LabelUi>,
 )
 
-// FIXME: 시간과 관련된 데이터를 처리하는 로직은 수정 해주세요
 internal fun Diary.toDiaryUi(): DiaryUi =
     DiaryUi(
         id = id,
         title = title,
         content = content,
-        createdAt = Instant.now().toDisplayableDateTime(),
-        updatedAt = Instant.now().toDisplayableDateTime(),
+        createdAt = createdAt.toDisplayableDateTime(),
+        updatedAt = updatedAt.toDisplayableDateTime(),
         images = images,
         labels = labels.map { it.toLabelUi() },
     )
