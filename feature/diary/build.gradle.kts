e617e82bf6643c71e25f2dbd90e9688fe7957a86
flashdragon <yooh3574@naver.com>
2024-11-07 22:51:58 +0900

    feat: 의존성

--- feature/diary/build.gradle.kts
+++ feature/diary/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(project(":core:data"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
