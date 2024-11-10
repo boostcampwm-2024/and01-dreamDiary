c4a11ab5d0a74cb8422cc8c3c7ac2e13b09f185f
ruh0n <wns8256@naver.com>
2024-11-10 21:01:18 +0900

    feat: 캘린더 탭에 달력 연결

--- feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/home/components/DiaryCalendarTab.kt
+++ feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/home/components/DiaryCalendarTab.kt
@@ -2,8 +2,18 @@ package com.boostcamp.dreamteam.dreamdiary.feature.diary.home.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.boostcamp.dreamteam.dreamdiary.designsystem.theme.DreamdiaryTheme
 
 @Composable
 internal fun DiaryCalendarTab(modifier: Modifier = Modifier) {
-    // TODO: Calendar 탭 구현
+    DiaryCalendar(modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DiaryCalendarTabPreview() {
+    DreamdiaryTheme {
+        DiaryCalendarTab()
+    }
 }
