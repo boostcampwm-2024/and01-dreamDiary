709d350263149eea98c9b6f957d6ceba6d9ed2ec
ruh0n <wns8256@naver.com>
2024-11-10 21:00:53 +0900

    feat: 달력이 더 잘 보이도록 변경

--- feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/home/components/DiaryCalendarBody.kt
+++ feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/home/components/DiaryCalendarBody.kt
@@ -3,6 +3,7 @@ package com.boostcamp.dreamteam.dreamdiary.feature.diary.home.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -10,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.boostcamp.dreamteam.dreamdiary.designsystem.theme.DreamdiaryTheme
 import java.time.DayOfWeek
@@ -50,6 +53,7 @@ internal fun DiaryCalendarBody(
                 modifier = Modifier.fillMaxWidth(),
             )
         }
+        HorizontalDivider()
     }
 }
 
@@ -62,12 +66,18 @@ private fun WeekHeader(
 
     Row(modifier = modifier) {
         reorderedDays.forEach { i ->
-            DayCell(modifier = Modifier.weight(1f)) {
+            DayCell(
+                modifier = Modifier.weight(1f),
+                minHeight = 24.dp,
+            ) {
                 Text(
                     text = DayOfWeek.of(i).getDisplayName(
                         TextStyle.SHORT,
                         locale,
                     ),
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleMedium,
                 )
             }
         }
@@ -82,7 +92,9 @@ private fun WeekRow(
 ) {
     Row(modifier = modifier) {
         for (i in 0 until 7) {
-            DayCell(modifier = Modifier.weight(1f)) {
+            DayCell(
+                modifier = Modifier.weight(1f),
+            ) {
                 val day = firstDateOfWeek.plusDays(i.toLong())
                 Text(
                     text = (day.dayOfMonth).toString(),
@@ -91,6 +103,7 @@ private fun WeekRow(
                     } else {
                         MaterialTheme.colorScheme.onSurface
                     },
+                    fontWeight = FontWeight.SemiBold,
                     style = MaterialTheme.typography.bodyMedium,
                 )
             }
@@ -101,10 +114,12 @@ private fun WeekRow(
 @Composable
 private fun DayCell(
     modifier: Modifier = Modifier,
+    minHeight: Dp = 48.dp,
     content: @Composable () -> Unit = {},
 ) {
     Column(
-        modifier = modifier,
+        modifier = modifier.defaultMinSize(minHeight = minHeight),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         content()
