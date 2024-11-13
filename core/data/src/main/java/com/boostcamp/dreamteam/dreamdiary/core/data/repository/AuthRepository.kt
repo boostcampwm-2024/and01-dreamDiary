502a83a7f633dfd9cd7b1fd23a1c0ea0cee9e813
flashdragon <yooh3574@naver.com>
2024-11-13 18:39:03 +0900

    feat: 자동 로그인, 건너 뛰기

--- core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/repository/AuthRepository.kt
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/repository/AuthRepository.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
@@ -16,13 +19,20 @@ class AuthRepository @Inject constructor(
 ) {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
+    private var user: FirebaseUser? = Firebase.auth.currentUser
+
     suspend fun signInWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential).await()
+        user = auth.currentUser
     }
 
     suspend fun signOut() {
         CredentialManager.create(context).clearCredentialState(ClearCredentialStateRequest())
         auth.signOut()
+    }
+
+    fun getUser(): String? {
+        return user?.email
     }
 }
