d0b12529f1b352a14425837ed547e7eecf269602
ruh0n <wns8256@naver.com>
2024-11-14 09:12:01 +0900

    feat: android application compose convention plugin 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dreamdiary.android.application.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.google.gms.google.services)
@@ -9,18 +7,10 @@ plugins {
 
 android {
     namespace = "com.boostcamp.dreamteam.dreamdiary"
-    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.boostcamp.dreamteam.dreamdiary"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "0.0.1"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -30,27 +20,18 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     buildFeatures {
         buildConfig = true
-        compose = true
     }
 }
 
 dependencies {
+    implementation(projects.core.designsystem)
+    implementation(projects.feature.auth)
+    implementation(projects.feature.diary)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
     ksp(libs.hilt.compiler)
@@ -58,15 +39,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    coreLibraryDesugaring(libs.desugar.jdk.libs)
-
-    implementation(projects.feature.diary)
-    implementation(projects.feature.auth)
-    implementation(projects.core.designsystem)
 
     // Timber
     implementation(libs.timber)
