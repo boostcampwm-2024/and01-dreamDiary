d536841834d5b8828e80037e09eafd99bd0b28fa
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-15 16:57:13 +0900

    feat: room에서 꿈일기와 라벨을 함께 가져올 수 있는 함수 및 모델 추가

---
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/database/model/DreamDiaryWithLabels.kt
@@ -0,0 +1,19 @@
+package com.boostcamp.dreamteam.dreamdiary.core.data.database.model
+
+import androidx.room.Embedded
+import androidx.room.Junction
+import androidx.room.Relation
+
+data class DreamDiaryWithLabels(
+    @Embedded val dreamDiary: DreamDiaryEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "label",
+        associateBy = Junction(
+            value = DreamDiaryLabelEntity::class,
+            parentColumn = "diaryId",
+            entityColumn = "labelId",
+        )
+    )
+    val labels: List<LabelEntity>
+)
