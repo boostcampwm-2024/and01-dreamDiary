702398985c3c8d92a81b9a651bbe20eff1d027b0
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-18 01:55:11 +0900

    feat: 꿈일기를 추가할 때 DiaryContent를 받는 함수 및 꿈일기 조회 할 때 DiaryContent를 제공하는 함수 추가

--- core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/repository/DefaultDreamDiaryRepository.kt
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/repository/DefaultDreamDiaryRepository.kt
@@ -5,12 +5,16 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.map
 import com.boostcamp.dreamteam.dreamdiary.core.data.database.dao.DreamDiaryDao
+import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.ImageEntity
 import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.LabelEntity
+import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.TextEntity
 import com.boostcamp.dreamteam.dreamdiary.core.model.Diary
+import com.boostcamp.dreamteam.dreamdiary.core.model.DiaryContent
 import com.boostcamp.dreamteam.dreamdiary.core.model.Label
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import java.time.Instant
+import java.util.UUID
 import javax.inject.Inject
 
 internal class DefaultDreamDiaryRepository @Inject constructor(
@@ -32,13 +36,54 @@ internal class DefaultDreamDiaryRepository @Inject constructor(
         )
     }
 
+    override suspend fun addDreamDiary(
+        title: String,
+        diaryContents: List<DiaryContent>,
+        labels: List<String>,
+        sleepStartAt: Instant,
+        sleepEndAt: Instant
+    ) {
+        var body = ""
+        for (diaryContent in diaryContents) {
+            val newId = UUID.randomUUID().toString()
+            when (diaryContent) {
+                is DiaryContent.Text -> {
+                    dreamDiaryDao.insertText(
+                        TextEntity(
+                            newId,
+                            diaryContent.text
+                        )
+                    )
+                    body += "text:${newId}:"
+                }
+
+                is DiaryContent.Image -> {
+                    dreamDiaryDao.insertImage(
+                        ImageEntity(
+                            newId,
+                            diaryContent.path
+                        )
+                    )
+                    body += "image:${newId}:"
+                }
+            }
+        }
+        dreamDiaryDao.insertDreamDiary(
+            title = title,
+            body = body,
+            labels = labels,
+            sleepStartAt = sleepStartAt,
+            sleepEndAt = sleepEndAt,
+        )
+    }
+
     override fun getDreamDiaries(): Flow<PagingData<Diary>> =
         Pager(
             config = PagingConfig(pageSize = 100),
             pagingSourceFactory = { dreamDiaryDao.getDreamDiaries() },
         ).flow.map { pagingData ->
             pagingData.map {
-                it.toDomain()
+                it.toDomain(parseBody(it.body))
             }
         }
 
@@ -75,6 +120,40 @@ internal class DefaultDreamDiaryRepository @Inject constructor(
             .map { list -> list.map { it.toDomain() } }
 
     override suspend fun getDreamDiary(id: String): Diary {
-        return dreamDiaryDao.getDreamDiary(id).toDomain()
+        val dreamDiaryEntity = dreamDiaryDao.getDreamDiary(id)
+        return dreamDiaryEntity.toDomain(parseBody(dreamDiaryEntity.dreamDiary.body))
+    }
+
+    private suspend fun parseBody(body: String): List<DiaryContent> {
+        val diaryContents = mutableListOf<DiaryContent>()
+
+        val parsingDiaryContent = body.split(":")
+        var index = 0
+
+        while (index < parsingDiaryContent.size) {
+            if (parsingDiaryContent[index] == "text") {
+                index += 1
+                val id = parsingDiaryContent[index]
+                val textEntity = dreamDiaryDao.getText(id) ?: continue
+                diaryContents.add(
+                    DiaryContent.Text(
+                        text = textEntity.text
+                    )
+                )
+            } else if (parsingDiaryContent[index] == "image") {
+                index += 1
+                val id = parsingDiaryContent[index]
+                val imageEntity = dreamDiaryDao.getImage(id) ?: continue
+                diaryContents.add(
+                    DiaryContent.Image(
+                        path = imageEntity.path
+                    )
+                )
+            } else {
+                index += 1
+                continue
+            }
+        }
+        return diaryContents
     }
 }
