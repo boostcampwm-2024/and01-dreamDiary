5d37912bd6a3e539f465b506d02d6e4a9900015b
ruh0n <wns8256@naver.com>
2024-11-18 19:02:14 +0900

    feat: 꿈 일기 작성 화면의 이미지에 삭제 버튼 추가
    Co-authored-by: minwooch <79406120+minwooch@users.noreply.github.com>

--- feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/write/component/DiaryWriteScreenBody.kt
+++ feature/diary/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/diary/write/component/DiaryWriteScreenBody.kt
@@ -1,11 +1,21 @@
 package com.boostcamp.dreamteam.dreamdiary.feature.diary.write.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.outlined.Error
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,14 +23,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
+import coil3.compose.SubcomposeAsyncImage
 import coil3.request.ImageRequest
 import com.boostcamp.dreamteam.dreamdiary.designsystem.theme.DreamdiaryTheme
 import com.boostcamp.dreamteam.dreamdiary.feature.diary.R
@@ -91,45 +104,109 @@ private fun InputBody(
     Column(modifier = modifier) {
         diaryContents.forEachIndexed { index, diaryContent ->
             when (diaryContent) {
-                is DiaryContentUi.Image -> AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current).data(diaryContent.path).build(),
-                    contentDescription = stringResource(R.string.write_content_image),
+                is DiaryContentUi.Image -> BodyImage(
+                    diaryContent = diaryContent,
+                    onContentImageDelete = onContentImageDelete,
                     modifier = Modifier.fillMaxWidth(),
                 )
 
-                is DiaryContentUi.Text -> {
-                    var textFieldValueState by remember {
-                        mutableStateOf(TextFieldValue(diaryContent.text))
-                    }
-
-                    val textFieldValue = textFieldValueState.copy(text = diaryContent.text)
-
-                    BasicTextField(
-                        value = textFieldValue,
-                        onValueChange = {
-                            textFieldValueState = it
-
-                            onContentTextPositionChange(it.selection.end)
-                            onContentTextChange(index, it.text)
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .onFocusChanged { if (it.isFocused) onContentFocusChange(index) },
-                        textStyle = MaterialTheme.typography.bodyLarge,
-                        decorationBox = { innerTextField ->
-                            if (textFieldValue.text.isEmpty()) {
-                                Text(
-                                    text = stringResource(R.string.write_text_content),
-                                    style = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.secondary),
-                                )
-                            }
-                            innerTextField()
-                        },
-                    )
-                }
+                is DiaryContentUi.Text -> BodyText(
+                    diaryContent = diaryContent,
+                    onContentTextPositionChange = onContentTextPositionChange,
+                    onContentTextChange = onContentTextChange,
+                    index = index,
+                    onContentFocusChange = onContentFocusChange,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .onFocusChanged { if (it.isFocused) onContentFocusChange(index) },
+                )
             }
         }
     }
+}
+
+@Composable
+private fun BodyImage(
+    diaryContent: DiaryContentUi.Image,
+    onContentImageDelete: (DiaryContentUi) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        SubcomposeAsyncImage(
+            model = ImageRequest.Builder(LocalContext.current).data(diaryContent.path).build(),
+            contentDescription = stringResource(R.string.write_content_image),
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 200.dp),
+            error = {
+                if (LocalInspectionMode.current) {
+                    Image(
+                        painter = painterResource(R.drawable.google_icon),
+                        contentDescription = stringResource(R.string.write_content_image),
+                    )
+                } else {
+                    Icon(
+                        imageVector = Icons.Outlined.Error,
+                        contentDescription = stringResource(R.string.write_content_image),
+                    )
+                }
+            },
+        )
+        FilledIconButton(
+            onClick = { },
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(8.dp),
+            colors = IconButtonDefaults.filledIconButtonColors().copy(
+                containerColor = MaterialTheme.colorScheme.surfaceContainerHighest.copy(alpha = 0.8f),
+                contentColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
+            ),
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = stringResource(R.string.write_content_image_delete),
+            )
+        }
+    }
+}
+
+@Composable
+private fun BodyText(
+    diaryContent: DiaryContentUi.Text,
+    onContentTextPositionChange: (textPosition: Int) -> Unit,
+    onContentTextChange: (contentIndex: Int, String) -> Unit,
+    index: Int,
+    onContentFocusChange: (contentIndex: Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var textFieldValueState by remember {
+        mutableStateOf(TextFieldValue(diaryContent.text))
+    }
+
+    val textFieldValue = textFieldValueState.copy(text = diaryContent.text)
+
+    BasicTextField(
+        value = textFieldValue,
+        onValueChange = {
+            textFieldValueState = it
+
+            onContentTextPositionChange(it.selection.end)
+            onContentTextChange(index, it.text)
+        },
+        modifier = modifier,
+        textStyle = MaterialTheme.typography.bodyLarge,
+        decorationBox = { innerTextField ->
+            if (textFieldValue.text.isEmpty()) {
+                Text(
+                    text = stringResource(R.string.write_text_content),
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        color = MaterialTheme.colorScheme.secondary,
+                    ),
+                )
+            }
+            innerTextField()
+        },
+    )
 }
 
 @Preview(showBackground = true)
@@ -144,7 +221,7 @@ private fun DiaryWriteScreenBodyPreviewEmpty() {
             onContentFocusChange = { },
             onContentTextPositionChange = { },
             onContentImageDelete = { },
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
         )
     }
 }
@@ -156,12 +233,15 @@ private fun DiaryWriteScreenBodyPreviewFilled() {
         DiaryWriteScreenBody(
             title = "안녕 뉴라인",
             onTitleChange = {},
-            diaryContents = listOf(DiaryContentUi.Text("안녕\n뉴라인")),
+            diaryContents = listOf(
+                DiaryContentUi.Text("안녕\n뉴라인"),
+                DiaryContentUi.Image("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"),
+            ),
             onContentTextChange = { _, _ -> },
             onContentFocusChange = { },
             onContentTextPositionChange = { },
             onContentImageDelete = { },
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
         )
     }
 }
