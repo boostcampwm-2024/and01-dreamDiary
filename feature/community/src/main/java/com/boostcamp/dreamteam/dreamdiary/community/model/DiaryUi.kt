b407ac7e94456b378a4b2bbb71a837f711c8aeb3
ruh0n <wns8256@naver.com>
2024-11-19 19:38:15 +0900

    feat: preview 값 추가

--- feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/model/DiaryUi.kt
+++ feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/model/DiaryUi.kt
@@ -1,5 +1,23 @@
 package com.boostcamp.dreamteam.dreamdiary.community.model
 
 data class DiaryUi(
-    val id: String
+    val id: String,
+)
+
+internal val diaryUiPreview1 = DiaryUi(
+    id = "1",
+)
+
+private val diaryUiPreview2 = DiaryUi(
+    id = "2",
+)
+
+private val diaryUiPreview3 = DiaryUi(
+    id = "3",
+)
+
+internal val diariesUiPreview = listOf(
+    diaryUiPreview1,
+    diaryUiPreview2,
+    diaryUiPreview3,
 )
