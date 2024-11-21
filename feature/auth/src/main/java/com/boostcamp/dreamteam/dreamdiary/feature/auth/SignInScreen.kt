323b18e067f9c9daf638871ad1c5765e1f8a15d0
easyhak <jooin2000@cau.ac.kr>
2024-11-21 19:31:45 +0900

    feat: signIn screen 로고

--- feature/auth/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/auth/SignInScreen.kt
+++ feature/auth/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/auth/SignInScreen.kt
@@ -3,10 +3,9 @@ package com.boostcamp.dreamteam.dreamdiary.feature.auth
 import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -28,7 +26,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -152,11 +149,10 @@ private fun SignInScreenContent(
                     .padding(innerPadding),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
             ) {
-                Box(
-                    modifier = Modifier
-                        .size(60.dp)
-                        .clip(RoundedCornerShape(10.dp))
-                        .background(Color(130, 211, 224)),
+                Image(
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = stringResource(R.string.singIn_dream_diary_logo),
+                    modifier = Modifier.size(60.dp),
                 )
                 Text(
                     stringResource(R.string.signIn_dream_diary),
