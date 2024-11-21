014d3bdc8473481531d6cae9b10484eb434f2dda
easyhak <jooin2000@cau.ac.kr>
2024-11-21 19:58:43 +0900

    fix: google icon 사이즈 다른 것 수정

--- feature/auth/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/auth/SignInScreen.kt
+++ feature/auth/src/main/java/com/boostcamp/dreamteam/dreamdiary/feature/auth/SignInScreen.kt
@@ -4,6 +4,7 @@ import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -26,10 +27,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -151,11 +154,17 @@ private fun SignInScreenContent(
             ) {
                 Image(
                     painter = painterResource(R.drawable.logo),
+
                     contentDescription = stringResource(R.string.singIn_dream_diary_logo),
-                    modifier = Modifier.size(60.dp),
+                    modifier = Modifier
+                        .size(72.dp)
+                        .clip(MaterialTheme.shapes.medium)
+                        .background(Color.Black)
+                        .padding(8.dp),
                 )
                 Text(
                     stringResource(R.string.signIn_dream_diary),
+                    fontWeight = FontWeight.SemiBold,
                     fontSize = 40.sp,
                 )
             }
