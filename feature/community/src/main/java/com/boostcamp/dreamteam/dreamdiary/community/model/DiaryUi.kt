6019f37bb9a0b1bf859f2ad4b8c4d68c0f236548
ruh0n <wns8256@naver.com>
2024-11-22 22:14:44 +0900

    feat: 일기 데이터를 표시하기 위한 클래스 추가

--- feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/model/DiaryUi.kt
+++ feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/model/DiaryUi.kt
@@ -1,19 +1,50 @@
 package com.boostcamp.dreamteam.dreamdiary.community.model
 
+import com.boostcamp.dreamteam.dreamdiary.community.model.vo.DisplayableDateTime
+import com.boostcamp.dreamteam.dreamdiary.community.model.vo.toDisplayableDateTime
+import java.time.Instant
+import java.time.ZoneId
+
 data class DiaryUi(
     val id: String,
+    val thumbnail: String? = null,
+    val title: String,
+    val previewText: String,
+    val sharedAt: DisplayableDateTime,
+    val commentCount: Long,
+    val isLiked: Boolean,
+    val author: UserUi,
 )
 
 internal val diaryUiPreview1 = DiaryUi(
     id = "1",
+    title = "Diary 1",
+    previewText = "This is a preview text for Diary 1",
+    sharedAt = Instant.now().toDisplayableDateTime(zoneId = ZoneId.systemDefault()),
+    commentCount = 10,
+    isLiked = false,
+    author = userUiPreview1,
 )
 
-private val diaryUiPreview2 = DiaryUi(
+internal val diaryUiPreview2 = DiaryUi(
     id = "2",
+    thumbnail = "https://picsum.photos/200/300",
+    title = "Diary 2",
+    previewText = "This is a preview text for Diary 2",
+    sharedAt = Instant.now().toDisplayableDateTime(zoneId = ZoneId.systemDefault()),
+    commentCount = 2147483647,
+    isLiked = true,
+    author = userUiPreview2,
 )
 
 private val diaryUiPreview3 = DiaryUi(
     id = "3",
+    title = "Diary 3",
+    previewText = "This is a preview text for Diary 3",
+    sharedAt = Instant.now().toDisplayableDateTime(zoneId = ZoneId.systemDefault()),
+    commentCount = 0,
+    isLiked = false,
+    author = userUiPreview3,
 )
 
 internal val diariesUiPreview = listOf(
