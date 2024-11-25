af3af8db56b79ea1c2439a9a70c5f48812edaac7
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-25 16:42:03 +0900

    feat: 데이터베이스에 엔티티 추가

--- core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/database/DreamDiaryDatabase.kt
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/database/DreamDiaryDatabase.kt
@@ -8,10 +8,18 @@ import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.DreamDiaryEnt
 import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.DreamDiaryLabelEntity
 import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.ImageEntity
 import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.LabelEntity
+import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.SynchronizingDreamDiaryEntity
 import com.boostcamp.dreamteam.dreamdiary.core.data.database.model.TextEntity
 
 @Database(
-    entities = [DreamDiaryEntity::class, LabelEntity::class, DreamDiaryLabelEntity::class, TextEntity::class, ImageEntity::class],
+    entities = [
+        DreamDiaryEntity::class,
+        LabelEntity::class,
+        DreamDiaryLabelEntity::class,
+        TextEntity::class,
+        ImageEntity::class,
+        SynchronizingDreamDiaryEntity::class,
+    ],
     version = 1,
 )
 @TypeConverters(value = [InstantTypeConverter::class])
