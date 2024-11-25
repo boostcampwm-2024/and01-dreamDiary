bb4134e62909878956d6ae993364214b17731382
easyhak <jooin2000@cau.ac.kr>
2024-11-25 17:02:41 +0900

    feat: community data class 생성

---
+++ core/model/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/model/Comment.kt
@@ -0,0 +1,10 @@
+package com.boostcamp.dreamteam.dreamdiary.core.model
+
+import java.time.Instant
+
+data class Comment(
+    val author: String,
+    val content: String,
+    val likes: Int = 0,
+    val createdAt: Instant
+)
---
+++ core/model/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/model/CommunityDreamPost.kt
@@ -0,0 +1,15 @@
+package com.boostcamp.dreamteam.dreamdiary.core.model
+
+import java.time.Instant
+
+data class CommunityDreamPost(
+    val author: String,
+    val title: String,
+    val content: String,
+    val labels: List<Label> = emptyList(),
+    val comments: List<Comment> = emptyList(),
+    val likes: Int = 0,
+    val sleepStartAt: Instant,
+    val sleepEndAt: Instant,
+    val createdAt: Instant
+)
