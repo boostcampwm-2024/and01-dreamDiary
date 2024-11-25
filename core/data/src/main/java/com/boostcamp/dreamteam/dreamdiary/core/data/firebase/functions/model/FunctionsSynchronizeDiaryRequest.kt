5987029a989824e8ae3af5248be2a03bc7385eeb
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-25 22:19:18 +0900

    feat: functions needSync의 요청 모델 추가

---
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/firebase/functions/model/FunctionsSynchronizeDiaryRequest.kt
@@ -0,0 +1,40 @@
+package com.boostcamp.dreamteam.dreamdiary.core.data.firebase.functions.model
+
+import com.boostcamp.dreamteam.dreamdiary.core.model.synchronization.SynchronizeDreamDiaryRequest
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FunctionsSynchronizeDiaryRequest(
+    val list: List<DreamDiary>
+) {
+    @Serializable
+    data class DreamDiary(
+        val diaryId: String,
+        val title: String,
+        val content: String,
+        val createdAt: Long,
+        val updatedAt: Long,
+        val deletedAt: Long?,
+        val sleepStartAt: Long,
+        val sleepEndAt: Long,
+        val labels: List<String>,
+        val previousVersion: String,
+        val currentVersion: String,
+    )
+}
+
+fun SynchronizeDreamDiaryRequest.toFunctionsRequest(): FunctionsSynchronizeDiaryRequest.DreamDiary {
+    return FunctionsSynchronizeDiaryRequest.DreamDiary(
+        diaryId = this.id,
+        title = this.title,
+        content = this.content,
+        createdAt = this.createdAt.toEpochMilli(),
+        updatedAt = this.updatedAt.toEpochMilli(),
+        deletedAt = this.deletedAt?.toEpochMilli(),
+        sleepStartAt = this.sleepStartAt.toEpochMilli(),
+        sleepEndAt = this.sleepEndAt.toEpochMilli(),
+        labels = this.labels,
+        previousVersion = this.previousVersion,
+        currentVersion = this.currentVersion,
+    )
+}
