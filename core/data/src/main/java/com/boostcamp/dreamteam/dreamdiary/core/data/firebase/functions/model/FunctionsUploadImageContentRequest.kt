a43308d15d1d1ba82d18800ba3ee7e957da49064
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-26 03:02:44 +0900

    fix: 요청 모델에 Serializable 어노테이션 추가

--- core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/firebase/functions/model/FunctionsUploadImageContentRequest.kt
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/firebase/functions/model/FunctionsUploadImageContentRequest.kt
@@ -1,5 +1,8 @@
 package com.boostcamp.dreamteam.dreamdiary.core.data.firebase.functions.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FunctionsUploadImageContentRequest(
     val id: String,
     val path: String,
--- core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/firebase/functions/model/FunctionsUploadTextContentRequest.kt
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/firebase/functions/model/FunctionsUploadTextContentRequest.kt
@@ -1,5 +1,8 @@
 package com.boostcamp.dreamteam.dreamdiary.core.data.firebase.functions.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FunctionsUploadTextContentRequest(
     val id: String,
     val text: String,
