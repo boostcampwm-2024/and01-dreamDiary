ec969c490cc85ea42cb4ae0498d38d050e24aaec
minwooch <79406120+minwooch@users.noreply.github.com>
2024-11-25 22:20:32 +0900

    feat: functions needSync관련 도메인 모델 추가

---
+++ core/model/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/model/synchronization/SynchronizeDreamDiaryRequest.kt
@@ -0,0 +1,24 @@
+package com.boostcamp.dreamteam.dreamdiary.core.model.synchronization
+
+import java.time.Instant
+
+data class SynchronizeDreamDiaryRequest(
+    val id: String,
+    val title: String,
+    val content: String,
+    val createdAt: Instant,
+    val updatedAt: Instant,
+    val deletedAt: Instant?,
+    val sleepStartAt: Instant,
+    val sleepEndAt: Instant,
+    val labels: List<String>,
+    val diaryContents: List<ContentId>,
+    val previousVersion: String,
+    val currentVersion: String,
+) {
+    sealed class ContentId {
+        data class Image(val id: String) : ContentId()
+        data class Text(val id: String) : ContentId()
+    }
+}
+
