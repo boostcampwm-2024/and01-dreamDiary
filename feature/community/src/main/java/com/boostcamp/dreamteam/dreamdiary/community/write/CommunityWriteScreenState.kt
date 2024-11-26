544c9115792cb542271b05c06d3e814c38d5350e
ruh0n <wns8256@naver.com>
2024-11-26 23:57:24 +0900

    feat: 초기 uistate 구성 로직 추가

--- feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/write/CommunityWriteScreenState.kt
+++ feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/write/CommunityWriteScreenState.kt
@@ -1,6 +1,8 @@
 package com.boostcamp.dreamteam.dreamdiary.community.write
 
 import com.boostcamp.dreamteam.dreamdiary.community.model.vo.PostContentUi
+import com.boostcamp.dreamteam.dreamdiary.community.model.vo.toPostContentUi
+import com.boostcamp.dreamteam.dreamdiary.core.model.Diary
 
 data class CommunityWriteScreenState(
     val editorState: EditorState = EditorState(),
@@ -11,3 +13,9 @@ data class EditorState(
     val title: String = "",
     val contents: List<PostContentUi> = listOf(PostContentUi.Text("")),
 )
+
+internal fun Diary.toEditorState(): EditorState =
+    EditorState(
+        title = title,
+        contents = diaryContents.map { it.toPostContentUi() },
+    )
