4577d2e2db52e5e1e85b76c5edd262b21866cd12
easyhak <jooin2000@cau.ac.kr>
2024-11-27 19:42:14 +0900

    fix: auth repository 사용

--- core/domain/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/domain/usecase/community/AddCommentUseCase.kt
+++ core/domain/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/domain/usecase/community/AddCommentUseCase.kt
@@ -1,28 +1,30 @@
 package com.boostcamp.dreamteam.dreamdiary.core.domain.usecase.community
 
-import com.boostcamp.dreamteam.dreamdiary.core.data.dto.CommentRequest
+import com.boostcamp.dreamteam.dreamdiary.core.data.repository.AuthRepository
 import com.boostcamp.dreamteam.dreamdiary.core.data.repository.CommentRepository
 import com.boostcamp.dreamteam.dreamdiary.core.model.Author
 import javax.inject.Inject
 
 class AddCommentUseCase @Inject constructor(
     private val commentRepository: CommentRepository,
+    private val authRepository: AuthRepository
 ) {
     suspend operator fun invoke(
         postId: String,
         content: String,
     ) {
-        val userName = "author"
-        val profileImageUrl = "https://picsum.photos/200/300"
-        commentRepository.addComment(
+        val uid = authRepository.getUserUID() ?: throw Exception()
+        val userName = authRepository.getUserName() ?: throw Exception()
+        val profileImageUrl = authRepository.getUserPhotoUrl() ?: throw Exception()
+
+        commentRepository.saveComment(
             postId = postId,
-            commentRequest = CommentRequest(
-                content = content,
-                author = Author(
-                    userName = userName,
-                    profileImageUrl = profileImageUrl,
-                ),
-            ),
+            content = content,
+            author = Author(
+                id = uid,
+                userName = userName,
+                profileImageUrl = profileImageUrl.toString()
+            )
         )
     }
 }
