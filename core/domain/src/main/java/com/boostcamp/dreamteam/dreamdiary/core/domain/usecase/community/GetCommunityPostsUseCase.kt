c9cf8e318b6d25bae301dbe199710ed502273500
easyhak <jooin2000@cau.ac.kr>
2024-11-27 11:26:29 +0900

    fix: rename 이름 변경

--- core/domain/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/domain/usecase/community/GetCommunityPostsUseCase.kt
+++ core/domain/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/domain/usecase/community/GetCommunityPostsUseCase.kt
@@ -6,7 +6,7 @@ import com.boostcamp.dreamteam.dreamdiary.core.model.CommunityDreamPost
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCommunityPostUseCase @Inject constructor(
+class GetCommunityPostsUseCase @Inject constructor(
     private val communityRepository: CommunityRepository,
 ) {
     operator fun invoke(): Flow<PagingData<CommunityDreamPost>> {
--- feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/list/CommunityListViewModel.kt
+++ feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/list/CommunityListViewModel.kt
@@ -6,7 +6,7 @@ import androidx.paging.cachedIn
 import androidx.paging.map
 import com.boostcamp.dreamteam.dreamdiary.community.model.toPostUi
 import com.boostcamp.dreamteam.dreamdiary.core.domain.usecase.community.AddCommunityPostUseCase
-import com.boostcamp.dreamteam.dreamdiary.core.domain.usecase.community.GetCommunityPostUseCase
+import com.boostcamp.dreamteam.dreamdiary.core.domain.usecase.community.GetCommunityPostsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,7 +17,7 @@ import javax.inject.Inject
 @HiltViewModel
 class CommunityListViewModel @Inject constructor(
     private val addCommunityPostUseCase: AddCommunityPostUseCase,
-    private val getCommunityPostUseCase: GetCommunityPostUseCase,
+    private val getCommunityPostsUseCase: GetCommunityPostsUseCase,
 ) : ViewModel() {
     private val _state = MutableStateFlow(CommunityListUiState())
     val state = _state.asStateFlow()
@@ -33,7 +33,7 @@ class CommunityListViewModel @Inject constructor(
         }
     }
 
-    val posts = getCommunityPostUseCase()
+    val posts = getCommunityPostsUseCase()
         .map { pagingData ->
             pagingData.map { it.toPostUi() } // CommunityDreamPost -> PostUi 변환
         }
