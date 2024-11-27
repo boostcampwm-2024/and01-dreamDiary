1a565fedddb6aa6c4a1d818bed9f374e0d07b9b9
easyhak <jooin2000@cau.ac.kr>
2024-11-27 19:41:07 +0900

    feat: paging source 분리

---
+++ core/data/src/main/java/com/boostcamp/dreamteam/dreamdiary/core/data/firebase/FirebaseCommentPagingSource.kt
@@ -0,0 +1,45 @@
+package com.boostcamp.dreamteam.dreamdiary.core.data.firebase
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.boostcamp.dreamteam.dreamdiary.core.data.dto.CommentResponse
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
+
+class FirebaseCommentPagingSource(
+    private val commentReference: CollectionReference
+) : PagingSource<Query, CommentResponse>() {
+    override suspend fun load(params: LoadParams<Query>): LoadResult<Query, CommentResponse> {
+        return try {
+            val query = params.key ?: commentReference
+                .orderBy("createdAt", com.google.firebase.firestore.Query.Direction.DESCENDING)
+                .limit(params.loadSize.toLong())
+
+            val querySnapshot = query.get().await()
+            val documents = querySnapshot.documents
+            val comments = documents.map { it.toObject(CommentResponse::class.java)!! }
+
+            val nextQuery = if (documents.isNotEmpty()) {
+                query.startAfter(documents.last())
+            } else {
+                null
+            }
+
+            LoadResult.Page(
+                data = comments,
+                prevKey = null,
+                nextKey = nextQuery,
+            )
+        } catch (e: Exception) {
+            Timber.e(e, "Error loading comments")
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Query, CommentResponse>): Query? {
+        return null
+    }
+
+}
