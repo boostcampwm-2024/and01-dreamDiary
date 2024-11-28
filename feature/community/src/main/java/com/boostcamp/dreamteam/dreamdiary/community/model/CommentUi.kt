0335ddf22e57ca17768da3372027c66da81910f9
easyhak <jooin2000@cau.ac.kr>
2024-11-28 11:14:14 +0900

    rename: contents -> content

--- feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/model/CommentUi.kt
+++ feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/model/CommentUi.kt
@@ -6,28 +6,28 @@ import kotlinx.coroutines.flow.flowOf
 data class CommentUi(
     val id: String,
     val author: UserUi,
-    val contents: String,
+    val content: String,
     val isLiked: Boolean,
 )
 
 internal val commentUiPreview1 = CommentUi(
     id = "1",
     author = userUiPreview1,
-    contents = "댓글 내용",
+    content = "댓글 내용",
     isLiked = true,
 )
 
 internal val commentUiPreview2 = CommentUi(
     id = "2",
     author = userUiPreview2,
-    contents = "댓글 내용",
+    content = "댓글 내용",
     isLiked = false,
 )
 
 private val commentUiPreview3 = CommentUi(
     id = "3",
     author = userUiPreview3,
-    contents = "댓글 내용",
+    content = "댓글 내용",
     isLiked = true,
 )
 
