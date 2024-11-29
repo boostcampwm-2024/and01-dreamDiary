c73c01e7292acd4c92a0b67e9c72dd3a10546e16
easyhak <jooin2000@cau.ac.kr>
2024-11-30 00:02:25 +0900

    feat: commentContent 부분 uiState로 넣기

--- feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/detail/CommunityDetailUiState.kt
+++ feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/detail/CommunityDetailUiState.kt
@@ -5,4 +5,5 @@ import com.boostcamp.dreamteam.dreamdiary.community.model.PostDetailUi
 data class CommunityDetailUiState(
     val post: PostDetailUi = PostDetailUi.EMPTY,
     val isLoading: Boolean = true,
+    val commentContent: String = "",
 )
