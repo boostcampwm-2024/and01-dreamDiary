514ceb3c18bafd7980c407c2f379dd714665bd51
easyhak <jooin2000@cau.ac.kr>
2024-12-02 11:36:20 +0900

    feat: 버전 업

--- build-logic/convention/src/main/java/com/boostcamp/dreamteam/dreamdiary/buildlogic/convention/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/boostcamp/dreamteam/dreamdiary/buildlogic/convention/AndroidApplicationConventionPlugin.kt
@@ -19,8 +19,8 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                     minSdk = 24
                     targetSdk = 35
 
-                    versionCode = 1
-                    versionName = "0.0.1"
+                    versionCode = 2
+                    versionName = "0.0.2"
                 }
 
                 configureKotlinAndroid(this)
