df978913ee837f7277b29f6f9159815361baaef2
ruh0n <wns8256@naver.com>
2024-12-02 17:59:09 +0900

    feat: 댓글 모양 변경(유튜브 댓글 참고)

--- feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/detail/component/CommunityDetailComment.kt
+++ feature/community/src/main/java/com/boostcamp/dreamteam/dreamdiary/community/detail/component/CommunityDetailComment.kt
@@ -1,15 +1,26 @@
 package com.boostcamp.dreamteam.dreamdiary.community.detail.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.LocalContentColor
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,32 +35,56 @@ internal fun CommunityDetailComment(
     comment: CommentUi,
     modifier: Modifier = Modifier,
 ) {
-    CompositionLocalProvider(
-        LocalContentColor provides MaterialTheme.colorScheme.surface,
-        LocalTextStyle provides LocalTextStyle.current.copy(
-            color = MaterialTheme.colorScheme.onSurface,
-        ),
-    ) {
-        ListItem(
-            headlineContent = { Text(text = comment.content) },
-            modifier = modifier,
-            overlineContent = {
-                Text(
-                    text = comment.author.username,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                )
-            },
-            leadingContent = {
+    Surface(modifier = modifier) {
+        Box {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            ) {
                 DdAsyncImage(
                     model = comment.author.profileImageUrl,
                     contentDescription = null,
                     modifier = Modifier
-                        .size(40.dp)
+                        .size(24.dp)
                         .clip(MaterialTheme.shapes.extraLarge),
                     contentScale = ContentScale.Crop,
                 )
-            },
-        )
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(end = 12.dp),
+                ) {
+                    Text(
+                        text = comment.author.username,
+                        style = MaterialTheme.typography.labelMedium.copy(
+                            color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.8f),
+                        ),
+                        maxLines = 1,
+                    )
+                    Text(
+                        text = comment.content,
+                        style = MaterialTheme.typography.bodyMedium,
+                        lineHeight = LocalTextStyle.current.fontSize * 1.5f,
+                    )
+                }
+            }
+            IconButton(
+                onClick = { },
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .scale(0.7f),
+            ) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.6f),
+                )
+            }
+        }
     }
 }
 
