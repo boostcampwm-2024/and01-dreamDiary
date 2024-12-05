de66c85ca14b31657f0a2f0ca64c74588240693e
easyhak <jooin2000@cau.ac.kr>
2024-12-05 18:39:44 +0900

    fix: version 0.0.4로 변경

--- build-logic/convention/src/main/java/com/boostcamp/dreamteam/dreamdiary/buildlogic/convention/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/boostcamp/dreamteam/dreamdiary/buildlogic/convention/AndroidApplicationConventionPlugin.kt
@@ -19,8 +19,8 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                     minSdk = 24
                     targetSdk = 35
 
-                    versionCode = 3
-                    versionName = "0.0.3"
+                    versionCode = 4
+                    versionName = "0.0.4"
                 }
 
                 configureKotlinAndroid(this)
